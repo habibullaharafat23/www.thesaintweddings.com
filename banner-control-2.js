9f23f278e8bc4124e8c68b72f9214bb0dd3e0ed8
MD Habibullah Arafat <148046236+habibullaharafat23@users.noreply.github.com>
2025-01-29 23:17:32 +0600

    Update banner-control-2.js

--- banner-control-2.js
+++ banner-control-2.js
@@ -34,7 +34,7 @@ var cookieInfoText = ["Expiration", "Domain", "Controller"];
 
 var bannerShowingDelayed = 0;
 
-var websiteLogo = "https://cdn.prod.website-files.com/601987b8e4fc3d2bd8810278/601995093c81388e4d7de4d1_Saint%20Lavie%20Logo.svg";
+var websiteLogo = "#";
 
 var logoWidth = "50px";
 
